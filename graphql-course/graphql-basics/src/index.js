fd1aee7642cb989b0743467dcca961bf0c289f66
Tatsuro <contacts@tatsuro.dev>
2020-11-01 02:11:57 +0900

    scalar type array 16

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -6,7 +6,8 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
   type Query {
     greeting(name: String, position: String): String!
-    add(a: Float!, b: Float!): Float!
+    add(numbers: [Float!]!): Float!
+    grades: [Int!]!
     me: User!
     post: Post!
   }
@@ -37,7 +38,15 @@ const resolvers = {
       }
     },
     add(parent, args, ctx, info) {
-      return args.a + args.b
+      if (args.numbers.length === 0) {
+        return 0
+      }
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue
+      }, 0)
+    },
+    grades(parent, args, ctx, info) {
+      return [99, 80, 93]
     },
     me() {
       return {
