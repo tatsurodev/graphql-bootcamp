7cab23d23f2ca1690e6d53b8d83420ff4382856f
Tatsuro <contacts@tatsuro.dev>
2020-10-31 18:35:44 +0900

    es6 import, export 練習 10

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello GraphQLS')
+// import myCurrentLocation, { getGreeting, message, name } from './myModule'
+
+// console.log(message)
+// console.log(name)
+// console.log(myCurrentLocation)
+// console.log(getGreeting('Jessica'))
+
+import myAddFunction, { subtract } from './math'
+
+console.log(myAddFunction(1, -2))
+console.log(subtract(10, 2))
