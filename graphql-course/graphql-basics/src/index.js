2685ac167ea31fd833d38a2bc30468f661a13929
Tatsuro <contacts@tatsuro.dev>
2020-11-05 02:41:05 +0900

    delete post, comment mutation 29

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -89,7 +89,9 @@ const typeDefs = `
     createUser(data: CreateUserInput!): User!
     deleteUser(id: ID!): User!
     createPost(data: CreatePostInput!): Post!
+    deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput!): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   ${/*comment*/ ''}
@@ -249,6 +251,15 @@ const resolvers = {
       posts.push(post)
       return post
     },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex((post) => post.id === args.id)
+      if (postIndex === -1) {
+        throw new Error('Post not found')
+      }
+      const deletedPosts = posts.splice(postIndex, 1)
+      comments = comments.filter((comment) => comment.post !== args.id)
+      return deletedPosts[0]
+    },
     createComment(parent, args, ctx, info) {
       const userExists = users.some((user) => user.id === args.data.author)
       const postExists = posts.some(
@@ -263,6 +274,16 @@ const resolvers = {
       }
       comments.push(comment)
       return comment
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(
+        (comment) => comment.id === args.id
+      )
+      if (commentIndex === -1) {
+        throw new Error('Comment not found')
+      }
+      const deletedComments = comments.splice(commentIndex, 1)
+      return deletedComments[0]
     },
   },
   User: {
