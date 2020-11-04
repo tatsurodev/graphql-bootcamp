0f80c357fc01d079daab0b340e88ac7d663b36c0
Tatsuro <contacts@tatsuro.dev>
2020-11-04 22:14:34 +0900

    create user mutation 24

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
+import { v4 as uuidv4 } from 'uuid'
 
 // scalar types - String, Boolean, Int, Float, ID
 
@@ -83,6 +84,10 @@ const typeDefs = `
     post: Post!
   }
 
+  type Mutation {
+    createUser(name: String!, email: String!, age:Int): User!
+  }
+
   type User {
     id: ID!
     name: String!
@@ -151,6 +156,22 @@ const resolvers = {
         body: '',
         published: false,
       }
+    },
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.email)
+      if (emailTaken) {
+        throw new Error('Email taken.')
+      }
+      const user = {
+        id: uuidv4(),
+        name: args.name,
+        email: args.email,
+        age: args.age,
+      }
+      users.push(user)
+      return user
     },
   },
   User: {
