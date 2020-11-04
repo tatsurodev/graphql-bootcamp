9a2a141df60a499ea1914b397f3dc16439791d80
Tatsuro <contacts@tatsuro.dev>
2020-11-04 23:13:24 +0900

    create post, comment mutation 25

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -42,7 +42,7 @@ const posts = [
     id: '12',
     title: 'Programming Music',
     body: '',
-    published: false,
+    published: true,
     author: '2',
   },
 ]
@@ -86,6 +86,8 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age:Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -172,6 +174,39 @@ const resolvers = {
       }
       users.push(user)
       return user
+    },
+    createPost(parent, args, ctx, info) {
+      // 引数のuserが存在するかcheck
+      const userExists = users.some((user) => user.id === args.author)
+      if (!userExists) {
+        throw new Error('User not found')
+      }
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      }
+      posts.push(post)
+      return post
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author)
+      const postExists = posts.some(
+        (post) => post.id === args.post && post.published
+      )
+      if (!userExists || !postExists) {
+        throw new Error('Unable to find user and post')
+      }
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post,
+      }
+      comments.push(comment)
+      return comment
     },
   },
   User: {
