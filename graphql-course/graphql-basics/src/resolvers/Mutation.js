b8387ac8b30c9b41eb2e5f94160b3d293bd6a327
Tatsuro <contacts@tatsuro.dev>
2020-11-05 20:07:14 +0900

    comment subscription作成 39

--- graphql-course/graphql-basics/src/resolvers/Mutation.js
+++ graphql-course/graphql-basics/src/resolvers/Mutation.js
@@ -176,20 +176,31 @@ const Mutation = {
     }
     db.comments.push(comment)
     // commentが作成される度にそのcommentを該当のchannelにpublish
-    pubsub.publish(`comment ${args.data.post}`, { comment })
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: 'CREATED',
+        data: comment,
+      },
+    })
     return comment
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex(
       (comment) => comment.id === args.id
     )
     if (commentIndex === -1) {
       throw new Error('Comment not found')
     }
-    const deletedComments = db.comments.splice(commentIndex, 1)
-    return deletedComments[0]
+    const [deletedComment] = db.comments.splice(commentIndex, 1)
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: 'DELETED',
+        data: deletedComment,
+      },
+    })
+    return deletedComment
   },
-  updateComment(parent, args, { db }, info) {
+  updateComment(parent, args, { db, pubsub }, info) {
     const { id, data } = args
     const comment = db.comments.find((comment) => comment.id === id)
     if (!comment) {
@@ -198,6 +209,12 @@ const Mutation = {
     if (typeof data.text === 'string') {
       comment.text = data.text
     }
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: 'UPDATED',
+        data: comment,
+      },
+    })
     return comment
   },
 }
