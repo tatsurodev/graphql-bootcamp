cd8905380c9d02d1bfa6645f3c172ca3d717ebb5
Tatsuro <contacts@tatsuro.dev>
2020-11-05 19:45:38 +0900

    post subscriptionをcreate, update, delete用に拡張 38

--- graphql-course/graphql-basics/src/resolvers/Mutation.js
+++ graphql-course/graphql-basics/src/resolvers/Mutation.js
@@ -89,22 +89,36 @@ const Mutation = {
     }
     db.posts.push(post)
     if (args.data.published) {
-      pubsub.publish('post', { post })
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post,
+        },
+      })
     }
     return post
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id)
     if (postIndex === -1) {
       throw new Error('Post not found')
     }
-    const deletedPosts = db.posts.splice(postIndex, 1)
+    const [post] = db.posts.splice(postIndex, 1)
     db.comments = db.comments.filter((comment) => comment.post !== args.id)
-    return deletedPosts[0]
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post,
+        },
+      })
+    }
+    return post
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
     const { id, data } = args
     const post = db.posts.find((post) => post.id === id)
+    const originalPost = { ...post }
     if (!post) {
       throw new Error('Post not found')
     }
@@ -114,8 +128,37 @@ const Mutation = {
     if (typeof data.body === 'string') {
       post.body = data.body
     }
+    // 更新されるdataにpublishedが含まれる
     if (typeof data.published === 'boolean') {
       post.published = data.published
+      // published true->false
+      if (originalPost.published && !post.published) {
+        // deleted
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost,
+          },
+        })
+        // published false->true
+      } else if (!originalPost.published && post.published) {
+        // create
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post,
+          },
+        })
+      }
+      // 更新されるdataにpublishedが含まれない && 対象postのpublishedがtrue
+    } else if (post.published) {
+      // update
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post,
+        },
+      })
     }
     return post
   },
