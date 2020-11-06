b9977fb132c0f4a5b71de2a0a566d894f370222f
Tatsuro <contacts@tatsuro.dev>
2020-11-07 01:19:16 +0900

    prisma bindingでnodejsからmutationを実行 52

--- graphql-course/graphql-prisma/src/prisma.js
+++ graphql-course/graphql-prisma/src/prisma.js
@@ -6,6 +6,7 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466',
 })
 
+/* query
 // prisma.query, prisma.mutation, prisma.subscription, prisma.exists
 // mutation methodの第一引数はmutationの引数、第二引数はselection setでpromiseが返ってくる
 prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
@@ -18,3 +19,48 @@ prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
 prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
   console.log(JSON.stringify(data, undefined, 2))
 })
+*/
+
+prisma.mutation
+  .createPost(
+    {
+      data: {
+        title: 'GraphQL 101',
+        body: '',
+        published: false,
+        author: {
+          connect: {
+            id: 'ckh64rmdn004x0815qhoa3nd4',
+          },
+        },
+      },
+    },
+    '{ id title body published }'
+  )
+  .then((data) => {
+    console.log(data)
+    return prisma.query.users(null, '{ id name posts { id title } }')
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
+  })
+
+prisma.mutation
+  .updatePost(
+    {
+      where: {
+        id: 'ckh6eyckz03q50815cqg2sbqq',
+      },
+      data: {
+        body: 'This is how to get started with Graphql...',
+        published: true,
+      },
+    },
+    '{ id }'
+  )
+  .then((data) => {
+    return prisma.query.posts(null, '{ id title body published }')
+  })
+  .then((data) => {
+    console.log(data)
+  })
